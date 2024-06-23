2cd63f2c6727bf4a67d717dba88ed703d7f0c1a7
Teodor <teo.ruschev@gmail.com>
2024-06-23 20:00:15 +0300

    Added Framer Motion
    Added framer motion animations to the homepage.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
-import profilePhoto from './profilePhotoFinal.png'
+import profilePhoto from './profilePhotoFinal.png';
+import { motion } from 'framer-motion';
+import { appearDelayedChildren } from '../../utils/animations';
+
 export const Home = () => {
     return (
         <section className={styles["hero-container"]}>
-            <div className={styles["intro-container"]}>
-                <h1>Hi, I'm Teo - a web developer.<br/>Welcome to my domain.</h1>
+            <motion.div className={styles["intro-container"]}
+                variants={appearDelayedChildren}
+                initial="hidden"
+                animate="visible"
+                >
+                <h1>Hi, I'm Teo - a web developer.<br />Welcome to my domain.</h1>
                 <h3>See what I'm up to <strong className={styles["blogLink"]}><Link to={'/blog'}>here</Link></strong>.</h3>
-            </div>
+            </motion.div>
             <div className={styles["image-container"]}>
                 {/* <img src={profilePhoto} alt="" /> */}
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
+import { appearDown } from '../../utils/animations';
+import { motion } from 'framer-motion';
 
 export const Navigation = () => {
 
     return (
-        <div className={styles["nav-container"]}>
+        <motion.div className={styles["nav-container"]}
+            variants={appearDown}
+            initial="hidden"
+            animate="visible"
+        >
             <div className={styles["logo-container"]}>
                 <Link className={"logo-url"} to={"/"}>ruschev.dev_</Link>
             </div>
@@ -13,6 +19,6 @@ export const Navigation = () => {
                 <li className={styles["nav-item"]}><Link>Projects</Link></li>
                 <li className={styles["nav-item"]}><Link>Contact me</Link></li>
             </ul>
-        </div>
+        </motion.div>
     );
 }
